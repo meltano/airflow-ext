1093af986b1a2e97b3376b739356c5c5b09feb16
Florian Hines <syn@ronin.io>
2022-08-04 21:25:20 -0500

    Uber basic cookiecutter template

---
+++ cookiecutter/wrapper-template/{{cookiecutter.wrapper_id}}/{{cookiecutter.library_name}}/main.py
@@ -0,0 +1,111 @@
+"""{{ cookiecutter.source_name }} cli entrypoint."""
+
+import os
+import sys
+from typing import List
+
+import structlog
+import typer
+
+from {{ cookiecutter.library_name }}.wrapper import {{ cookiecutter.source_name }}
+
+from meltano_extension_sdk.extension import DescribeFormat
+from meltano_extension_sdk.logging import default_logging_config, parse_log_level
+
+log = structlog.get_logger()
+
+APP_NAME: str = "{{ cookiecutter.source_name }}"
+
+plugin =  {{ cookiecutter.source_name }}()
+
+app = typer.Typer(pretty_exceptions_enable=False)
+
+
+@app.command()
+def initialize(ctx: typer.Context, force: bool = False):
+    """Initialize the {{ cookiecutter.source_name }} plugin."""
+    try:
+        plugin.initialize(force)
+    except Exception:
+        log.exception(
+            "initialize failed with uncaught exception, please report exception to maintainer"
+        )
+        sys.exit(1)
+
+
+@app.command(
+    context_settings={"allow_extra_args": True, "ignore_unknown_options": True}
+)
+def invoke(ctx: typer.Context, command_args: List[str]):
+    """Invoke the plugin.
+
+    Note: that if a command argument is a list, such as command_args, then
+    unknown options are also included in the list and NOT stored in the context as usual.
+
+    Args:
+        ctx: The typer.Context for this invocation
+        command_args: The command args to invoke
+    """
+    command_name, command_args = command_args[0], command_args[1:]
+    log.debug(
+        "called", command_name=command_name, command_args=command_args, env=os.environ
+    )
+
+    try:
+        plugin.pre_invoke()
+    except Exception:
+        log.exception(
+            "pre_invoke failed with uncaught exception, please report exception to maintainer"
+        )
+        sys.exit(1)
+
+    try:
+        plugin.invoke(command_name, command_args)
+    except Exception:
+        log.exception(
+            "invoke failed with uncaught exception, please report exception to maintainer"
+        )
+        sys.exit(1)
+
+    try:
+        plugin.post_invoke()
+    except Exception:
+        log.exception(
+            "ppost_invoke failed with uncaught exception, please report exception to maintainer"
+        )
+        sys.exit(1)
+
+
+@app.command()
+def describe(
+    output_format: DescribeFormat = typer.Option(
+        DescribeFormat.text, "--format", help="Output format"
+    )
+):
+    """Describe the available commands of this extension."""
+    try:
+        typer.echo(plugin.describe_formatted(output_format))
+    except Exception:
+        log.exception(
+            "describe failed with uncaught exception, please report exception to maintainer"
+        )
+        sys.exit(1)
+
+
+@app.callback(invoke_without_command=True)
+def main(
+    ctx: typer.Context,
+    log_level: str = typer.Option("INFO", envvar="LOG_LEVEL"),
+    log_timestamps: bool = typer.Option(
+        False, envvar="LOG_TIMESTAMPS", help="Show timestamp in logs"
+    ),
+    log_levels: bool = typer.Option(
+        False, "--log-levels", envvar="LOG_LEVELS", help="Show log levels"
+    ),
+):
+    """
+    Simple Meltano extension that wraps the {{ cookiecutter.wrapper_target_name }} CLI.
+    """
+    default_logging_config(
+        level=parse_log_level(log_level), timestamps=log_timestamps, levels=log_levels
+    )
---
+++ cookiecutter/wrapper-template/{{cookiecutter.wrapper_id}}/{{cookiecutter.library_name}}/wrapper.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+import pkgutil
+import os
+import subprocess
+import sys
+
+import structlog
+
+from meltano_extension_sdk.extension import Description, ExtensionBase
+from meltano_extension_sdk.process import Invoker, log_subprocess_error
+
+log = structlog.get_logger()
+
+
+class {{ cookiecutter.source_name }}(ExtensionBase):
+
+    def __init__(self):
+        self.{{ cookiecutter.extension_name }}_bin = "{{ cookiecutter.wrapper_target_name }}" # verify this is the correct name
+        self.{{ cookiecutter.extension_name }} = Invoker(self.{{ cookiecutter.extension_name }}_bin, env=os.environ.copy())
+
+    def invoke(self, command_name: str | None, *command_args):
+        """Invoke the underlying cli, that is being wrapped by this extension."""
+        try:
+            self.airflow_invoker.run_and_log(command_name, *command_args)
+        except subprocess.CalledProcessError as err:
+            log_subprocess_error(
+                f"{{ cookiecutter.extension_name }} {command_name}", err, "{{ cookiecutter.extension_name }} invocation failed"
+            )
+            sys.exit(1)
+
+    def describe(self) -> Description:
+        """Return a description of the extension."""
+        # simply list what custom commands you'd like to make available
+        # or use ":splat" to indicate that you're extension supports wild card pass-through.
+        return Description(commands=[":splat"])
