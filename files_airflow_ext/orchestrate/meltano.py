a1306a6ec8cdca7440552b5fecc6c4de83b1ec66
Florian Hines <syn@ronin.io>
2022-09-07 18:30:46 -0400

    start_date may not be present in jobs (#14)

--- files_airflow_ext/orchestrate/meltano.py
+++ files_airflow_ext/orchestrate/meltano.py
@@ -124,12 +124,7 @@ def _meltano_job_generator(schedules):
         common_tags.append(f"job:{schedule['job']['name']}")
         interval = schedule["cron_interval"]
         args = DEFAULT_ARGS.copy()
-
-        if schedule["start_date"]:
-            args["start_date"] = schedule["start_date"]
-        else:
-            # Default to epoch; as it's not set to catch-up, so will only run once.
-            args["start_date"] = datetime(1970, 1, 1, 0, 0, 0)
+        args["start_date"] = schedule.get("start_date", datetime(1970, 1, 1, 0, 0, 0))
 
         with DAG(
             base_id,
